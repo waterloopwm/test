46fc21c2d8795ce9aeaae52d83dfee7543366de8
waterloopwm <waterloopwm@126.com>
2019-07-20 13:36:21 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
+require_once './vendor/autoload.php';
+use think\Db;;
+// 数据库配置信息设置（全局有效）
+Db::setConfig([
+        // 数据库类型
+        'type'        => 'mysql',
+        // 数据库连接DSN配置
+        'dsn'         => '',
+        // 服务器地址
+        'hostname'    => '127.0.0.1',
+        // 数据库名
+        'database'    => 'test',
+        // 数据库用户名
+        'username'    => 'root',
+        // 数据库密码
+        'password'    => 'root',
+        // 数据库连接端口
+        'hostport'    => '3306',
+        // 数据库连接参数
+        'params'      => [],
+        // 数据库编码默认采用utf8
+        'charset'     => 'utf8',
+        // 数据库表前缀
+        'prefix'      => '',
+]);
+
 set_exception_handler(function ($exception) {
     echo $exception->getMessage();
 });
@@ -31,18 +57,12 @@ function getCombinationToString($arr, $m)
     unset($temp_first2);
     return $result;
 }
-//双色球-红色任选10个数字
-$redArr           = ['01','02','03','04','05','06','07','08','09','10'];
-$redCombinationArr = getCombinationToString($redArr, 6); //10个数里面,取出6个数有多少种组合(即不考虑顺序)
-//echo "<pre>";
-//print_r($redCombinationArr);
-//echo "</pre>";
-//$data['count'] = count($result); //组合种数
-//$data['data']  = $result; //各种数据组合
-//echo "<pre>";
-//print_r($data);
-//echo "</pre>";
-//双色球-蓝色任选3个数字
+
+/*
+//双色球-红色任选6个数字,可以多选1个
+$redArr           = ['01','02','03','04','05','06','07','08'];
+$redCombinationArr = getCombinationToString($redArr, 6); //8个数里面,取出6个数有多少种组合(即不考虑顺序)
+//双色球-蓝色任选1个数字,可以多选1个
 $reArr=[];
 $blueArr=['01','02','03'];
 foreach ($blueArr as $v) {
@@ -50,17 +70,53 @@ foreach ($blueArr as $v) {
         $reArr[]=$value.','.$v;
     }
 }
+*/
+
+
+//大乐透-红色任选5个数字,可以多选2个
+$redArr           = ['01','02','03','04','05','06','07'];
+$redCombinationArr = getCombinationToString($redArr, 5); //7个数里面,取出5个数有多少种组合(即不考虑顺序)
+//大乐透-蓝色任选2个数字,可以多选2个
+$reArr=[];
+$blueArr=['01','02','03','04'];
+$blueCombinationArr = getCombinationToString($blueArr, 2); //4个数里面,取出2个数有多少种组合(即不考虑顺序)
+foreach ($blueCombinationArr as $v) {
+    foreach ($redCombinationArr as $value) {
+        $reArr[]=$value.','.$v;
+    }
+}
+
+
+echo count($reArr);
 $reStr=implode("|",$reArr);
 
-$connection = mysql_connect("127.0.0.1", "root", "root");//连接到数据库
-mysql_query("set names 'utf8'");//编码转化
-if (!$connection) {
-    die("could not connect to the database.\n" . mysql_error());//诊断连接错误
+/*foreach ($reArr as $value) {
+    $data[]=['bet_content'=>$value];
 }
-$selectedDb = mysql_select_db("test");//选择数据库
-if (!$selectedDb) {
-    die("could not to the database\n" . mysql_error());
+Db::name("bet")->insertAll($data);*/
+//$data=['bet_content'=>$reStr];
+//Db::name("bet")->insert($data);
+$one=['1','2'];
+$two=['3','4'];
+$three=['5','6'];
+$four=['7','8'];
+$five=['9','10'];
+$six=['11','12'];
+$seven=['13','14'];
+foreach ($one as $v) {
+    foreach ($two as $vv) {
+        foreach ($three as $vvv) {
+            foreach ($four as $vvvv) {
+                foreach ($five as $vvvvv) {
+                    foreach ($six as $vvvvvv) {
+                        foreach ($seven as $vvvvvvv) {
+                            $data[]=$v.$vv.$vvv.$vvvv.$vvvvv.$vvvvvv.$vvvvvvv;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
-$insertSql = "insert into bet(bet_content) values('$reStr')";
-$result = mysql_query($insertSql);
-echo $result . "\n";
+echo '<pre>';
+print_r($data);
